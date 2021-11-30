ebc596161aea9bb9f9cb16b33403006bbf4b8698
aviveldan <aviveldan@gmail.com>
2021-11-30 20:00:28 +0200

    added more funcs

--- hw2_winter22/Solution.py
+++ hw2_winter22/Solution.py
@@ -13,9 +13,12 @@ def createTables():
     try:
         conn = Connector.DBConnector()
         conn.execute("CREATE TABLE Teams(id integer UNIQUE)")
-        conn.execute("CREATE TABLE Matches(id integer UNIQUE NOT NULL, competition TEXT NOT NULL, home_id integer NOT NULL, away_id integer NOT NULL )")
-        conn.execute("CREATE TABLE Players(id integer UNIQUE NOT NULL, team_id integer UNIQUE NOT NULL, age integer NOT NULL, height integer NOT NULL, preferred_foot TEXT NOT NULL)")
-        conn.execute("CREATE TABLE Stadiums(id integer UNIQUE NOT NULL, capacity integer NOT NULL, belong_to integer UNIQUE)")
+        conn.execute(
+            "CREATE TABLE Matches(id integer UNIQUE NOT NULL, competition TEXT NOT NULL, home_id integer NOT NULL, away_id integer NOT NULL )")
+        conn.execute(
+            "CREATE TABLE Players(id integer UNIQUE NOT NULL, team_id integer UNIQUE NOT NULL, age integer NOT NULL, height integer NOT NULL, preferred_foot TEXT NOT NULL)")
+        conn.execute(
+            "CREATE TABLE Stadiums(id integer UNIQUE NOT NULL, capacity integer NOT NULL, belong_to integer UNIQUE)")
 
 
 
@@ -102,7 +105,6 @@ def addTeam(teamID: int) -> ReturnValue:
     if teamID <= 0:
         return ReturnValue.BAD_PARAMS
 
-
     conn = None
     try:
         conn = Connector.DBConnector()
@@ -126,8 +128,8 @@ def addTeam(teamID: int) -> ReturnValue:
 
 
 def addMatch(match: Match) -> ReturnValue:
-    id, competition, home_id, away_id  = match.getMatchID(), match.getCompetition(), match.getHomeTeamID(), match.getAwayTeamID()
-    if id <= 0 or home_id <= 0 or away_id <=0 or away_id == home_id:
+    id, competition, home_id, away_id = match.getMatchID(), match.getCompetition(), match.getHomeTeamID(), match.getAwayTeamID()
+    if id <= 0 or home_id <= 0 or away_id <= 0 or away_id == home_id:
         return ReturnValue.BAD_PARAMS
     if competition not in ['International', 'Domestic']:
         return ReturnValue.BAD_PARAMS
@@ -135,7 +137,10 @@ def addMatch(match: Match) -> ReturnValue:
     conn = None
     try:
         conn = Connector.DBConnector()
-        query = sql.SQL("INSERT INTO Matches(id, competition, home_id, away_id) VALUES({id}, {competition}, {home_id}, {away_id})").format(id=sql.Literal(id), competition=sql.Literal(competition), home_id=sql.Literal(home_id), away_id=sql.Literal(away_id))
+        query = sql.SQL(
+            "INSERT INTO Matches(id, competition, home_id, away_id) VALUES({id}, {competition}, {home_id}, {away_id})").format(
+            id=sql.Literal(id), competition=sql.Literal(competition), home_id=sql.Literal(home_id),
+            away_id=sql.Literal(away_id))
         rows_effected, _ = conn.execute(query)
     except DatabaseException.ConnectionInvalid as e:
         return ReturnValue.ERROR
@@ -152,7 +157,6 @@ def addMatch(match: Match) -> ReturnValue:
     finally:
         conn.close()
         return ReturnValue.OK
-
 
 
 def getMatchProfile(matchID: int) -> Match:
@@ -217,27 +221,166 @@ def deleteMatch(match: Match) -> ReturnValue:
     finally:
         conn.close()
         return r
-
     pass
 
 
 def addPlayer(player: Player) -> ReturnValue:
-    pass
+    id, team_id, age, height, foot = player.getPlayerID(), player.getTeamID(), player.getAge(), player.getHeight(), player.getFoot()
+    if id <= 0 or team_id <= 0 or height <= 0 or (foot not in ['Left', 'Right']):
+        return ReturnValue.BAD_PARAMS
+    if id is None or team_id is None or age is None or height is None or foot is None:
+        return ReturnValue.BAD_PARAMS
+
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "INSERT INTO Players(id, team_id, age, height, preferred_foot) VALUES({id}, {team_id}, {age}, {height}, {preferred_foot})").format(
+            id=sql.Literal(id), team_id=sql.Literal(team_id), age=sql.Literal(age), height=sql.Literal(height),
+            preferred_foot=sql.Literal(foot))
+        rows_effected, _ = conn.execute(query)
+    except DatabaseException.ConnectionInvalid as e:
+        return ReturnValue.ERROR
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        return ReturnValue.ALREADY_EXISTS
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+        return ReturnValue.OK
 
 
 def getPlayerProfile(playerID: int) -> Player:
+    conn = None
+    m = Player.badPlayer()
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT * FROM Players WHERE id = {id}").format(
+            id=sql.Literal(playerID))
+        rows_effected, res = conn.execute(query)
+        m = Player(res.rows[0][0], res.rows[0][1], res.rows[0][2], res.rows[0][3], res.rows[0][4])
+    except DatabaseException.ConnectionInvalid as e:
+        return m
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        return m
+    except DatabaseException.CHECK_VIOLATION as e:
+        return m
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        return m
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        return m
+    except Exception as e:
+        return m
+    finally:
+        conn.close()
+        return m
+    return m
     pass
 
 
+
 def deletePlayer(player: Player) -> ReturnValue:
+    conn = None
+    r = ReturnValue.OK
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "DELETE FROM Players WHERE id = {id}").format(
+            id=sql.Literal(player.getPlayerID()))
+        rows_effected, res = conn.execute(query)
+        if rows_effected == 0:
+            r = ReturnValue.NOT_EXISTS
+
+    except DatabaseException.ConnectionInvalid as e:
+        print(e)
+        r = ReturnValue.ERROR
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+        r = ReturnValue.ERROR
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+        r = ReturnValue.ERROR
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+        r = ReturnValue.ERROR
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+        r = ReturnValue.ERROR
+    except Exception as e:
+        print(e)
+        r = ReturnValue.ERROR
+    finally:
+        conn.close()
+        return r
     pass
 
 
 def addStadium(stadium: Stadium) -> ReturnValue:
-    pass
+    id, capacity, belong_to = stadium.getStadiumID(), stadium.getCapacity(), stadium.getBelongsTo()
+    if id <= 0 or capacity <= 0 or belong_to <= 0:
+        return ReturnValue.BAD_PARAMS
+    if capacity is None or belong_to is None:
+        return ReturnValue.BAD_PARAMS
+
+
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "INSERT INTO Stadiums(id, capacity, belong_to) VALUES({id}, {capacity}, {belong_to})").format(
+            id=sql.Literal(id), capacity=sql.Literal(capacity), belong_to=sql.Literal(belong_to))
+        rows_effected, _ = conn.execute(query)
+    except DatabaseException.ConnectionInvalid as e:
+        return ReturnValue.ERROR
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        return ReturnValue.ALREADY_EXISTS
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+        return ReturnValue.OK
+
 
 
 def getStadiumProfile(stadiumID: int) -> Stadium:
+    conn = None
+    m = Stadium.badStadium()
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT * FROM Stadium WHERE id = {id}").format(
+            id=sql.Literal(stadiumID))
+        rows_effected, res = conn.execute(query)
+        m = Stadium(res.rows[0][0], res.rows[0][1], res.rows[0][2])
+    except DatabaseException.ConnectionInvalid as e:
+        return m
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        return m
+    except DatabaseException.CHECK_VIOLATION as e:
+        return m
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        return m
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        return m
+    except Exception as e:
+        return m
+    finally:
+        conn.close()
+        return m
+    return m
     pass
 
 
@@ -297,8 +440,8 @@ def getClosePlayers(playerID: int) -> List[int]:
     pass
 
 
-
 if __name__ == '__main__':
     # print("0. Creating all tables")
     # createTables()
-    deleteMatch(Match(1,'Domestic',3,9))
+    addStadium(Stadium(2,3,5))
+
