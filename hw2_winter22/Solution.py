c3f5f1f246a030be74bdb1528a683f694c083d47
aviveldan <aviveldan@gmail.com>
2021-12-04 14:36:17 +0200

    Reached PopularTeams. Taking a rest until will have a clear idea.

--- hw2_winter22/Solution.py
+++ hw2_winter22/Solution.py
@@ -15,19 +15,33 @@ def createTables():
         conn = Connector.DBConnector()
         conn.execute("CREATE TABLE Teams(id integer PRIMARY KEY, CHECK(id>0))")
         conn.execute(
-            "CREATE TABLE Matches(id integer PRIMARY KEY NOT NULL CHECK(id>0), competition TEXT NOT NULL CHECK(competition = 'International' OR competition = 'Domestic' ), home_id integer NOT NULL CHECK(home_id>0), away_id integer NOT NULL CHECK(away_id>0), FOREIGN KEY (home_id) REFERENCES Teams(id) ON DELETE CASCADE, FOREIGN KEY (away_id) REFERENCES Teams(id) ON DELETE CASCADE)")
+            "CREATE TABLE Matches(id integer PRIMARY KEY NOT NULL CHECK(id>0), competition TEXT NOT NULL CHECK(competition = 'International' OR competition = 'Domestic' ), home_id integer NOT NULL CHECK(home_id>0), away_id integer NOT NULL CHECK(away_id>0 AND away_id != home_id), FOREIGN KEY (home_id) REFERENCES Teams(id) ON DELETE CASCADE, FOREIGN KEY (away_id) REFERENCES Teams(id) ON DELETE CASCADE)")
         conn.execute(
-            "CREATE TABLE Players(id integer PRIMARY KEY NOT NULL CHECK(id>0), team_id integer NOT NULL CHECK(team_id>0), age integer NOT NULL, height integer NOT NULL, preferred_foot TEXT NOT NULL CHECK(preferred_foot = 'Left' OR preferred_foot = 'Right'),FOREIGN KEY (team_id) REFERENCES Teams(id) ON DELETE CASCADE)")
+            "CREATE TABLE Players(id integer PRIMARY KEY NOT NULL CHECK(id>0), team_id integer NOT NULL CHECK(team_id>0), age integer NOT NULL CHECK(age>0), height integer NOT NULL CHECK(height>0), preferred_foot TEXT NOT NULL CHECK(preferred_foot = 'Left' OR preferred_foot = 'Right'),FOREIGN KEY (team_id) REFERENCES Teams(id) ON DELETE CASCADE)")
         conn.execute(
             "CREATE TABLE Stadiums(id integer PRIMARY KEY NOT NULL CHECK(id>0), capacity integer NOT NULL CHECK(capacity>0), belong_to integer UNIQUE, FOREIGN KEY (belong_to) REFERENCES Teams(id))")
         conn.execute(
             "CREATE TABLE Scores(match_id integer, player_id integer, goals integer CHECK(goals>=0), FOREIGN KEY (match_id) REFERENCES Matches(id), FOREIGN KEY (player_id) REFERENCES Players(id))")
         conn.execute(
-            "CREATE TABLE Attendance(match_id integer, stadium_id integer, attendance integer CHECK(attendance>=0), FOREIGN KEY (match_id) REFERENCES Matches(id) ON DELETE CASCADE, FOREIGN KEY (stadium_id) REFERENCES Players(id) ON DELETE CASCADE)")
+            "CREATE TABLE Attendance(match_id integer UNIQUE, stadium_id integer, attendance integer CHECK(attendance>=0), FOREIGN KEY (match_id) REFERENCES Matches(id) ON DELETE CASCADE, FOREIGN KEY (stadium_id) REFERENCES Players(id) ON DELETE CASCADE)")
         conn.execute("CREATE VIEW AverageAttendance AS SELECT stadium_id, AVG(attendance) FROM Attendance GROUP BY stadium_id")
-        conn.execute("CREATE VIEW TotalStadiumGoals AS SELECT stadium_id, SUM(goals) FROM Scores GROUP BY stadium_id") #TODO fix it with join...
+        # Scores JOIN Matches by match_id JOIN stadiums by belong_to
+        conn.execute("CREATE VIEW TotalStadiumGoals AS  SELECT Attendance.stadium_id as s_id ,sum(Scores.goals) as total_goals FROM Attendance JOIN Scores ON Attendance.match_id=Scores.match_id GROUP BY Attendance.stadium_id")
         conn.execute("CREATE VIEW TotalMatchGoals AS SELECT match_id as m_id, SUM(goals) as sum_goals FROM Scores GROUP BY match_id")
-        conn.execute("CREATE VIEW Winners AS SELECT match_id, player_id, goals WHERE goals >= 0.5 *(SELECT sum(sum_goals) FROM TotalMatchGoals WHERE m_id = match_id)")
+        conn.execute("CREATE VIEW Winners AS SELECT Scores.match_id, Scores.player_id FROM Scores WHERE Scores.goals >= 0.5 *(SELECT sum(sum_goals) FROM TotalMatchGoals WHERE TotalMatchGoals.m_id = Scores.match_id)")
+        conn.execute(
+            "CREATE VIEW ActiveTeams AS SELECT Matches.home_id as id FROM Matches UNION SELECT Matches.away_id as id FROM Matches")
+        conn.execute(
+            "CREATE VIEW ActiveTallTeams AS SELECT team_id, count(team_id) as total FROM players JOIN activeteams ON players.team_id = activeteams.id WHERE players.height>190 GROUP BY team_id")
+        conn.execute(
+            "CREATE VIEW RichTeams AS SELECT belong_to FROM Stadiums WHERE capacity > 55000")
+        conn.execute(
+            "CREATE VIEW ActiveTallRichTeams AS SELECT team_id, total FROM ActiveTallTeams JOIN RichTeams ON ActiveTallTeams.team_id = RichTeams.belong_to ")
+        conn.execute(
+            "CREATE VIEW PossiblePopularTeams AS SELECT teams.id as team_id FROM Teams WHERE NOT EXISTS(SELECT * FROM matches LEFT JOIN attendance ON matches.id = attendance.match_id WHERE home_id = teams.id AND match_id is NULL)")
+        conn.execute("CREATE VIEW AttendanceTeams AS SELECT home_id, attendance FROM Attendance JOIN Matches ON Attendance.match_id = Matches.id")
+        conn.execute("CREATE VIEW PopularTeams AS SELECT team_id FROM PossiblePopularTeams WHERE Exists (select * from AttendanceTeams WHERE home_id = team_id AND attendance > 44000) AND NOT Exists (select * from AttendanceTeams WHERE home_id = team_id AND attendance <= 44000)")
+        conn.execute("CREATE VIEW neverPlayedTeams AS SELECT teams.id FROM teams WHERE NOT Exists (SELECT * FROM Matches WHERE home_id = teams.id OR away_id = teams.id)")
 
     except DatabaseException.ConnectionInvalid as e:
         print(e)
@@ -149,6 +163,7 @@ def addMatch(match: Match) -> ReturnValue:
             m_id=sql.Literal(m_id), competition=sql.Literal(competition), home_id=sql.Literal(home_id),
             away_id=sql.Literal(away_id))
         rows_effected, _ = conn.execute(query)
+
     except DatabaseException.ConnectionInvalid as e:
         r = ReturnValue.ERROR
     except DatabaseException.NOT_NULL_VIOLATION as e:
@@ -496,8 +511,8 @@ def matchInStadium(match: Match, stadium: Stadium, attendance: int) -> ReturnVal
     try:
         conn = Connector.DBConnector()
         query = sql.SQL(
-            "INSERT INTO Attendance(match_id, stadium_id, attendance) VALUES({match_id}, {stadium_id}, {count})").format(
-            match_id=sql.Literal(match.getMatchID()), stadium_id=sql.Literal(stadium.getStadiumID()), count=sql.Literal(attendance))
+            "INSERT INTO Attendance(match_id, stadium_id, attendance) VALUES({match_id}, {stadium_id}, {am})").format(
+            match_id=sql.Literal(match.getMatchID()), stadium_id=sql.Literal(stadium.getStadiumID()), am=sql.Literal(attendance))
         rows_effected, _ = conn.execute(query)
     except DatabaseException.ConnectionInvalid as e:
         r = ReturnValue.ERROR
@@ -562,13 +577,13 @@ def averageAttendanceInStadium(stadiumID: int) -> float:
     try:
         conn = Connector.DBConnector()
         query = sql.SQL(
-            "SELECT * FROM AverageAttendance WHERE stadium_id = {stadiumID}").format(
-            id=sql.Literal(stadiumID))
+            "SELECT avg FROM AverageAttendance WHERE stadium_id = {stadiumID}").format(
+            stadiumID=sql.Literal(stadiumID))
         rows_effected, res = conn.execute(query)
         if rows_effected == 0:
             m = 0
         else:
-            m = res.rows[0][1]
+            m = res.rows[0][0]
     except DatabaseException.ConnectionInvalid as e:
         m=-1
     except DatabaseException.NOT_NULL_VIOLATION as e:
@@ -589,23 +604,153 @@ def averageAttendanceInStadium(stadiumID: int) -> float:
 
 
 def stadiumTotalGoals(stadiumID: int) -> int:
+    conn = None
+    m = 0
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT total_goals FROM TotalStadiumGoals WHERE s_id = {stadiumID}").format(
+            stadiumID=sql.Literal(stadiumID))
+        rows_effected, res = conn.execute(query)
+        if rows_effected == 0:
+            m = 0
+        else:
+            m = res.rows[0][0]
+    except DatabaseException.ConnectionInvalid as e:
+        m=-1
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        m=-1
+    except DatabaseException.CHECK_VIOLATION as e:
+        m=-1
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        m=-1
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        m=-1
+    except Exception as e:
+        m=-1
+    finally:
+        conn.close()
+        return m
+    return m
     pass
+
 
 
 def playerIsWinner(playerID: int, matchID: int) -> bool:
-    pass
+    conn = None
+    m = False
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT * FROM Winners WHERE player_id={pID} AND match_id = {mID}").format(pID=sql.Literal(playerID), mID=sql.Literal(matchID))
+        rows_effected, res = conn.execute(query)
+        if(rows_effected>0):
+            m = True
+
+    except DatabaseException.ConnectionInvalid as e:
+        m = False
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        m = False
+    except DatabaseException.CHECK_VIOLATION as e:
+        m = False
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        m = False
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        m = False
+    except Exception as e:
+        m = False
+    finally:
+        conn.close()
+        return m
 
 
 def getActiveTallTeams() -> List[int]:
-    pass
+    conn = None
+    m = []
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT team_id FROM ActiveTallTeams WHERE total>=2 ORDER BY team_id DESC LIMIT 5")
+        rows_effected, res = conn.execute(query)
+        for i in range(len(res.rows)):
+            m.append(res.rows[i][0])
+
+    except DatabaseException.ConnectionInvalid as e:
+        m = []
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        m = []
+    except DatabaseException.CHECK_VIOLATION as e:
+        m = []
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        m = []
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        m = []
+    except Exception as e:
+        m = []
+    finally:
+        conn.close()
+        return m
 
 
 def getActiveTallRichTeams() -> List[int]:
-    pass
+    conn = None
+    m = []
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT team_id FROM ActiveTallRichTeams WHERE total>=2 ORDER BY team_id LIMIT 5")
+        rows_effected, res = conn.execute(query)
+        for i in range(len(res.rows)):
+            m.append(res.rows[i][0])
+
+    except DatabaseException.ConnectionInvalid as e:
+        m = []
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        m = []
+    except DatabaseException.CHECK_VIOLATION as e:
+        m = []
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        m = []
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        m = []
+    except Exception as e:
+        m = []
+    finally:
+        conn.close()
+        return m
+
+
 
 
 def popularTeams() -> List[int]:
-    pass
+    conn = None
+    m = []
+    try:
+        conn = Connector.DBConnector()
+        query = sql.SQL(
+            "SELECT id FROM PopularTeams UNION SELECT id FROM neverPlayedTeams ORDER BY id DESC LIMIT 10")
+        rows_effected, res = conn.execute(query)
+        for i in range(len(res.rows)):
+            m.append(res.rows[i][0])
+
+    except DatabaseException.ConnectionInvalid as e:
+        m = []
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        m = []
+    except DatabaseException.CHECK_VIOLATION as e:
+        m = []
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        m = []
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        m = []
+    except Exception as e:
+        m = []
+    finally:
+        conn.close()
+        return m
+
+
+
 
 
 def getMostAttractiveStadiums() -> List[int]:
